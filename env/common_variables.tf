4fd6d6c817ff8fc51e59bd43b0ccffb1e59f6cbb
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-05-10 23:42:31 -0400

    fmt common_variables.tf

--- env/common_variables.tf
+++ env/common_variables.tf
@@ -1,15 +1,15 @@
 
-variable account_id {
+variable "account_id" {
   description = "(Required) The account ID to perform actions on."
   type        = string
 }
 
-variable env {
+variable "env" {
   description = "(Required) The current running environment"
   type        = string
 }
 
-variable region { 
+variable "region" {
   description = "(Required) The region to build infra in"
-  type = string
+  type        = string
 }
