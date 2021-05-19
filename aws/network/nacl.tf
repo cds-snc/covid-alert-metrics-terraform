1687c9eda35f8736467646daa19f167ce10ee11b
Pat Heard <patrick.heard@cds-snc.ca>
2021-05-19 14:20:48 +0000

    feat: add main VPC Network ACL
    Removes the ingress/egress rules from the VPC's default NACL and
    puts them in a new VPC NACL resource.

--- aws/network/nacl.tf
+++ aws/network/nacl.tf
@@ -1,14 +1,20 @@
 resource "aws_default_network_acl" "default" {
   default_network_acl_id = aws_vpc.main.default_network_acl_id
 
-  subnet_ids = [aws_subnet.private.id, aws_subnet.public.id]
-
   tags = {
     Name = "${var.name}_default_nacl"
   }
+}
+
+resource "aws_network_acl" "main" {
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [aws_subnet.private.id, aws_subnet.public.id]
+
+  tags = {
+    Name = "${var.name}_main_nacl"
+  }
 
   ingress {
-
     rule_no    = 100
     protocol   = "tcp"
     action     = "allow"
@@ -19,7 +25,6 @@ resource "aws_default_network_acl" "default" {
   }
 
   ingress {
-
     rule_no    = 101
     protocol   = "tcp"
     action     = "allow"
@@ -30,7 +35,6 @@ resource "aws_default_network_acl" "default" {
   }
 
   egress {
-
     rule_no    = 102
     protocol   = -1
     action     = "allow"
