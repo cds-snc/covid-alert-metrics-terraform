04d72a596456abbad765a92b92851b66529a4e5f
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-05-21 09:11:28 -0400

    feat: add network infra for efs

---
+++ aws/network/efs_sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "efs" { 
+  description = "Used by EFS"
+  vpc_id = aws_vpc.main.id
+
+  tags = { 
+    Name = "${var.name}_efs_sg"
+  }
+
+  lifecycle { 
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "nfs_ingress" {
+  description              = "Allow communication to NFS"
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.efs.id
+  source_security_group_id = aws_security_group.lambda.id
+}
