15a261dc012994ea0d3498b16cc1a45448356ebe
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-05-26 16:23:40 -0400

    chore: autoformat

--- aws/dynamodb/outputs.tf
+++ aws/dynamodb/outputs.tf
@@ -2,7 +2,7 @@ output "raw_metrics_arn" {
   value = aws_dynamodb_table.raw_metrics.arn
 }
 
-output "raw_metrics_stream_arn" { 
+output "raw_metrics_stream_arn" {
   value = aws_dynamodb_table.raw_metrics.stream_arn
 }
 
--- aws/sqs/outputs.tf
+++ aws/sqs/outputs.tf
@@ -2,10 +2,10 @@ output "metrics_key_arn" {
   value = aws_kms_key.metrics_key.arn
 }
 
-output "dead_letter_queue_arn" { 
+output "dead_letter_queue_arn" {
   value = aws_sqs_queue.aggregation_lambda_dead_letter.arn
 }
 
-output "dead_letter_queue_url" { 
+output "dead_letter_queue_url" {
   value = aws_sqs_queue.aggregation_lambda_dead_letter.id
 }
