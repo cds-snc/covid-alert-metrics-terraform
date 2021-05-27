fa6328c978d53db30b1d5c02a5e79ba0eddb3b78
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-05-27 15:19:15 -0400

    fix: debug

--- scripts/comment_plan.js
+++ scripts/comment_plan.js
@@ -1,5 +1,7 @@
 module.exports = ({github, context, steps, matrix}) => {
 
+  console.log(JSON.stringify(steps, null, ' '));
+  console.log(JSON.stringify(matrix, null, ' '));
   const fmtOutcome = steps.fmtOutcome
   const planOutcome = steps.planOutcome
 
