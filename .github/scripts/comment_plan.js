34939d18c6e3ca075f9b73085332345a92c87799
Pat Heard <patrick.heard@cds-snc.ca>
2021-05-28 13:00:41 +0000

    fix: capitalize environment name

--- .github/scripts/comment_plan.js
+++ .github/scripts/comment_plan.js
@@ -1,21 +1,21 @@
 module.exports = (github, context, steps, matrix) => {
 
-  const fmtOutcome = steps.fmtOutcome
-  const planOutcome = steps.planOutcome
+  const fmtOutcome = steps.fmtOutcome;
+  const planOutcome = steps.planOutcome;
 
-  const deletes = steps.deletes
-  const creates = steps.creates
-  const updates = steps.updates
+  const deletes = steps.deletes;
+  const creates = steps.creates;
+  const updates = steps.updates;
 
   const deleteWarning = deletes === '0' ? '' : '**⚠️ &nbsp; WARNING:** resources will be destroyed by this change!';
 
-  const environment = matrix.environment
-  const moduleName = matrix.moduleName
+  const environment = matrix.environment[0].toUpperCase() + matrix.environment.substring(1);
+  const moduleName = matrix.moduleName;
 
   const iconFormat = fmtOutcome === 'success' ? '✅' : '❌';
   const iconPlan = planOutcome === 'success' ? '✅' : '❌';
 
-  const output = `## <span style="text-transform:uppercase">${ environment }</span> Module: ${ moduleName }
+  const output = `## ${ environment }: ${ moduleName }
 
 **${iconFormat} &nbsp; Terraform Format:** \`${ fmtOutcome }\`
 **${iconPlan} &nbsp; Terraform Plan:** \`${ planOutcome }\`  
