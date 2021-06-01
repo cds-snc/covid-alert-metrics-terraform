a42273cbee325c72c3e8c7b2e39096f4320c9686
Pat Heard <patrick.heard@cds-snc.ca>
2021-06-01 17:27:30 +0000

    feat: add Checkov skip for existing tfsec skips

--- aws/network/lambda_sg.tf
+++ aws/network/lambda_sg.tf
@@ -18,7 +18,7 @@ resource "aws_security_group_rule" "inet_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:AWS007  required to pull in lambda's data
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.lambda.id
 }
 
