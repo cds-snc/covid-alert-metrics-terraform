993c0e87332fa89cb6f1cc0bde082b713ba6cd9b
Pat Heard <patrick.heard@cds-snc.ca>
2021-06-08 16:58:17 +0000

    feat: more Checkov check skip comments
    - ECR: encryption with service key
    - S3: encryption with service key
    - S3: MFA delete
    - S3: cross-region replication

--- aws/modules/s3/s3.tf
+++ aws/modules/s3/s3.tf
@@ -2,6 +2,9 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "masked_metrics" {
   # checkov:skip=CKV_AWS_21:Versioning on this resource is handled through git
+  # checkov:skip=CKV_AWS_52:MFA delete only works with versioning
+  # checkov:skip=CKV_AWS_144:Cross-region replication not required (source of record for data is GitHub)
+  # checkov:skip=CKV_AWS_145:Using default service key for encryption is acceptable
   bucket = var.name
 
   server_side_encryption_configuration {
