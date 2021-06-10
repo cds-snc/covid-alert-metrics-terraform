5385829967c2632fb409599db1cd6cf39626dd1e
Pat Heard <patrick.heard@cds-snc.ca>
2021-06-10 13:26:15 +0000

    fix: get region and account ID from common vars

--- aws/backoff_retry_lambda/data_lookups.tf
+++ aws/backoff_retry_lambda/data_lookups.tf
@@ -1,6 +1,3 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
 data "aws_vpc" "covid_alert" {
   filter {
     name   = "tag:Name"
--- aws/backoff_retry_lambda/iam_policies.tf
+++ aws/backoff_retry_lambda/iam_policies.tf
@@ -65,7 +65,7 @@ data "aws_iam_policy_document" "backoff_retry" {
     ]
 
     resources = [
-      "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:network-interface/*"
+      "arn:aws:ec2:${var.region}:${var.account_id}:network-interface/*"
     ]
 
   }
