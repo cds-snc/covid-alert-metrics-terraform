2a793acb16e6ef08bbd0ec751a562f62f067a786
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-06-16 16:46:34 -0400

    ci: temporarily ignore checkov

--- aws/ecs/cloudwatch_event_role.tf
+++ aws/ecs/cloudwatch_event_role.tf
@@ -13,6 +13,7 @@ data "aws_iam_policy_document" "scheduled_task_cw_event_role_cloudwatch_policy" 
   statement {
     effect    = "Allow"
     actions   = ["ecs:RunTask"]
+    #checkov:skip=CKV_AWS_111:for testing only
     resources = ["*"]
   }
   statement {
