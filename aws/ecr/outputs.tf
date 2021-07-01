8720c722718eb228f7c1b0f118057feb0b6389b1
CalvinRodo <calvin.rodo@cds-snc.ca>
2021-07-01 14:56:48 -0400

    feat: add new container repositories
    Adds a container repo for the key server metrics etl task and the
    appstore metrics etl task.

--- aws/ecr/outputs.tf
+++ aws/ecr/outputs.tf
@@ -5,3 +5,19 @@ output "create_csv_repository_url" {
 output "create_csv_repository_arn" {
   value = aws_ecr_repository.create_csv.arn
 }
+
+output "server_metrics_etl_repository_url" {
+  value = aws_ecr_repository.server_metrics_etl.repository_url
+}
+
+output "server_metrics_etl_repository_arn" {
+  value = aws_ecr_repository.server_metrics_etl.arn
+}
+
+output "appstore_metrics_etl_repository_url" {
+  value = aws_ecr_repository.appstore_metrics_etl.repository_url
+}
+
+output "appstore_metrics_etl_repository_arn" {
+  value = aws_ecr_repository.appstore_metrics_etl.arn
+}
