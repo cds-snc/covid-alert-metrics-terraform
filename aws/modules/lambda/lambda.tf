db1f9a4845c295d3067a5c6ba1ef69ffb09cd94c
Pat Heard <patrick.heard@cds-snc.ca>
2021-07-19 19:20:39 +0000

    chore: remove EFS for Lambda module
    Lambda module no longer requires an EFS mounted.

--- aws/modules/lambda/lambda.tf
+++ aws/modules/lambda/lambda.tf
@@ -1,7 +1,6 @@
 
 locals {
   image_uri  = "${var.repository_url}:${var.tag}"
-  mount_path = "/mnt/efs"
 }
 
 resource "aws_lambda_function" "lambda" {
@@ -30,13 +29,6 @@ resource "aws_lambda_function" "lambda" {
       },
     var.env_variables)
   }
-
-  file_system_config {
-    arn              = aws_efs_access_point.access_point_for_lambda.arn
-    local_mount_path = local.mount_path
-  }
-
-  depends_on = [aws_efs_mount_target.mt]
 }
 
 resource "aws_cloudwatch_log_group" "metric_log" {
