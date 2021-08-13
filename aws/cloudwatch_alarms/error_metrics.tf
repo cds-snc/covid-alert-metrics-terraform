3f3136cb0a5a2cf26258b465dd834f453732f629
Pat Heard <patrick.heard@cds-snc.ca>
2021-08-13 14:53:07 -0400

    feat: set QR parse error alarm to warning topic (#199)
    Since QR is not in production, this does not need to be a
    critical alarm that creates an OpsGenie alert.

--- aws/cloudwatch_alarms/error_metrics.tf
+++ aws/cloudwatch_alarms/error_metrics.tf
@@ -10,9 +10,9 @@ resource "aws_cloudwatch_metric_alarm" "metrics_app_errors_500_qr_parse_above_cr
   namespace           = "CovidAlertApp"
   statistic           = "Sum"
   threshold           = var.app_500_qr_parse_error_critical_threshold
-  alarm_description   = "This metric monitors error-500-qr-parse errors in the covid alert app and generates a critical alarm"
+  alarm_description   = "This metric monitors error-500-qr-parse errors in the covid alert app and generates a warning alarm"
 
-  alarm_actions = [data.aws_sns_topic.alert_critical.arn]
+  alarm_actions = [data.aws_sns_topic.alert_warning.arn]
   dimensions = {
     Identifier = "error-500-qr-parse"
   }
