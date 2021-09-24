da9e58b22b8ffceb0973d6400955e9071999e487
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2021-09-24 13:27:30 -0400

    feat: release 2.1.0 payload splitting (#220)
    * feat: release 2.1.0 payload splitting
    
    * chore: copy missing test files
    
    * chore: hcl update

--- env/production/create_metrics_lambda/lambda/create_metric.js
+++ env/production/create_metrics_lambda/lambda/create_metric.js
@@ -21,31 +21,79 @@ exports.handler = async (event, context) => {
     // expire after 24 hours
     const ttl = (Math.floor(Date.now()/1000) + 86400).toString();
 
-    const params = {
-        TableName: process.env.TABLE_NAME,
-        Item : {
-            "uuid": {
-                S: uuidv4(),
-            },
-            "expdate" : {
-                N: ttl,
-            },
-            "raw": {
-                S: event.body,
-            },
-        },
-    };
-
     try {
+        // The maximum item size in DynamoDB is 400 KB
+        const payloadLength = new TextEncoder().encode(JSON.stringify(event.body.payload)).length;
+        if (payloadLength > process.env.SPLIT_THRESHOLD){
+            console.info(`Large payload being split; size: ${payloadLength}`);
 
-        await dynamodb.putItem(params).promise();
+            if(!Array.isArray(event.body.payload) || event.body.payload.length === 1){
+                console.error(`Upload failed, unable to split large payload: ${payloadLength} > ${process.env.SPLIT_THRESHOLD}`);
+                transactionStatus.statusCode = 200;
+                transactionStatus.body= JSON.stringify({ "status" : "RECORD DROPPED" }); 
+                return transactionStatus;
+            }
+            const results = splitPayload(event.body.payload);
+
+            for(const result of results){
+                let eventBody = {
+                    ...event.body
+                };
+                eventBody.payload = result;
+                await writePayload(JSON.stringify(eventBody), ttl);
+            };
+        }else{
+            await writePayload(event.body, ttl);
+        }
         transactionStatus.statusCode = 200;
         transactionStatus.body = JSON.stringify({ "status": "RECORD CREATED" });
     } catch (err) {
-        console.error(`Upload faile ${err}`);
+        console.error(`Upload failed ${err}`);
         transactionStatus.statusCode = 500;
         transactionStatus.body= JSON.stringify({ "status" : "UPLOAD FAILED" });
     }
 
     return transactionStatus;
+};
+
+// Recursively splits the payload in half until all chunks are below the limit
+const splitPayload = (payload) => {
+  const results = [];
+  let chunk_size = payload.length;
+  while (true) {
+    let left = payload.slice(0, chunk_size);
+    if (new TextEncoder().encode(JSON.stringify(left)).length < process.env.SPLIT_THRESHOLD) {
+     break;
+    }
+  
+    chunk_size /= 2;
+    // Can't go smaller then a single element chunk
+    if (chunk_size <= 1) {
+      break;
+    }
+  }
+  
+  for (let i = 0; i < payload.length; i += chunk_size) {
+    results.push(payload.slice(i, i + chunk_size));
+  }
+  return results;
+};
+
+const  writePayload = async (payload, ttl) => {
+  const params = {
+      TableName: process.env.TABLE_NAME,
+      Item : {
+          "uuid": {
+              S: uuidv4(),
+          },
+          "expdate" : {
+              N: ttl,
+          },
+          "raw": {
+              S: payload,
+          },
+      },
+  };
+  
+  return dynamodb.putItem(params).promise();
 };
--- env/production/create_metrics_lambda/lambda/create_metric.test.js
+++ env/production/create_metrics_lambda/lambda/create_metric.test.js
@@ -2,6 +2,7 @@
 
 const { handler } = require("./create_metric")
 const AWS = require('aws-sdk');
+const fs = require('fs');
 
 jest.mock("aws-sdk", () => {
   const mockDynamoDb = {
@@ -37,6 +38,55 @@ describe("handler", () => {
     })
   })
 
+  it("returns a 500 error code if the putItem fails when processing split payloads", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 50;
+
+    client.promise = jest.fn(async () => {throw "Error"})
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_large.json', 'utf8'))}
+
+    const response = await handler(event)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 500,
+      body: JSON.stringify({ "status" : "UPLOAD FAILED" })
+    })
+  })
+
+  it("returns a 500 error code if large single payload can't be split", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 50;
+
+    client.promise = jest.fn(async () => {throw "Error"})
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_large_single.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 500,
+      body: JSON.stringify({ "status" : "UPLOAD FAILED" })
+    })
+  })
+
+  it("returns a 200 error code if payload array has one element and can't be split", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 10;
+
+    client.promise = jest.fn(async () => true)
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_small.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ "status" : "RECORD DROPPED" })
+    })
+  })
+
   it("returns a 200 code if the putItem succeeds", async () => {
     client.promise = jest.fn(async () => true)
 
@@ -52,10 +102,11 @@ describe("handler", () => {
 
   it("saves the event body with a random UUID and a TTL", async () => {
     process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 307200;
 
     client.promise = jest.fn(async () => true)
 
-    const event = {body: ""}
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_small.json', 'utf8'))}
     const response = await handler(event)
 
     expect(client.putItem).toHaveBeenCalledWith(expect.objectContaining({
@@ -74,5 +125,72 @@ describe("handler", () => {
     })
 
     delete process.env.TABLE_NAME
+    delete process.env.SPLIT_THRESHOLD
+  })
+
+  it("saves large event body successfully after splitting", async () => {
+    
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 307200;
+
+    client.promise = jest.fn(async () => true)
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_gigantic.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(client.putItem).toHaveBeenCalledTimes(32)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ "status" : "RECORD CREATED" })
+    })
+
+    delete process.env.TABLE_NAME
+    delete process.env.SPLIT_THRESHOLD
+  })
+
+  it("saves large event body successfully after splitting in half", async () => {
+    
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 1000;
+
+    client.promise = jest.fn(async () => true)
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_large.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(client.putItem).toHaveBeenCalledTimes(2)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ "status" : "RECORD CREATED" })
+    })
+
+    delete process.env.TABLE_NAME
+    delete process.env.SPLIT_THRESHOLD
+  })
+
+  it("does not split if file is below split threshold", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 307200;
+
+    client.promise = jest.fn(async () => true)
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_large.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(client.putItem).toHaveBeenCalledTimes(1)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ "status" : "RECORD CREATED" })
+    })
+
+    delete process.env.TABLE_NAME
+    delete process.env.SPLIT_THRESHOLD
   })
 })
+
