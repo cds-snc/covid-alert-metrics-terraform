7ffdfb7ba7e71c5d3eb1a3e2a7ebec3d3df99ee6
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2021-09-28 16:47:42 -0400

    feat: stage pr metrics lambda (#228)
    * pr metrics lambda
    
    * chore: fix tests

--- env/production/create_metrics_lambda/lambda/create_metric.test.js
+++ env/production/create_metrics_lambda/lambda/create_metric.test.js
@@ -9,18 +9,25 @@ jest.mock("aws-sdk", () => {
       putItem: jest.fn().mockReturnThis(),
       promise: jest.fn(),
   };
+  const mockS3 = {
+    putObject: jest.fn().mockReturnThis(),
+    promise: jest.fn(),
+  };
   return {
       __esModule: true,
       DynamoDB: jest.fn(() => mockDynamoDb),
+      S3: jest.fn(() => mockS3),
   };
 });
 
 
 describe("handler", () => {
   let client
+  let s3Client
 
   beforeAll(async (done) => {
     client = new AWS.DynamoDB();
+    s3Client = new AWS.S3();
     jest.spyOn(console, 'error').mockImplementation(jest.fn());
     done();
    });
@@ -29,6 +36,23 @@ describe("handler", () => {
     client.promise = jest.fn(async () => {throw "Error"})
 
     const event = {body: ""}
+    const response = await handler(event)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 500,
+      body: JSON.stringify({ "status" : "UPLOAD FAILED" })
+    })
+  })
+
+  it("returns a 500 error code if the putObject fails", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.BUCKET_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 1;
+
+    s3Client.promise = jest.fn(async () => {throw "Error"})
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_small.json', 'utf8'))}
     const response = await handler(event)
 
     expect(response).toStrictEqual({
@@ -58,8 +82,10 @@ describe("handler", () => {
   it("returns a 200 error code if large single payload can't be split", async () => {
     process.env.TABLE_NAME = "foo"
     process.env.SPLIT_THRESHOLD = 50;
+    process.env.BUCKET_NAME = "bar"
 
     client.promise = jest.fn(async () => {throw "Error"})
+    s3Client.promise = jest.fn(async () => true)
 
     const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_large_single.json', 'utf8'))}
     const response = await handler(event)
@@ -100,6 +126,23 @@ describe("handler", () => {
     })
   })
 
+  it("returns a 200 code if the putObject succeeds", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 1;
+    process.env.BUCKET_NAME = "bar"
+
+    s3Client.promise = jest.fn(async () => true)
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_small.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ "status" : "RECORD DROPPED" })
+    })
+  })
+
   it("saves the event body with a random UUID and a TTL", async () => {
     process.env.TABLE_NAME = "foo"
     process.env.SPLIT_THRESHOLD = 307200;
@@ -126,6 +169,33 @@ describe("handler", () => {
 
     delete process.env.TABLE_NAME
     delete process.env.SPLIT_THRESHOLD
+  })
+
+  it("writes the event body to S3 if it's too large with random UUID as the key", async () => {
+    process.env.TABLE_NAME = "foo"
+    process.env.SPLIT_THRESHOLD = 1;
+    process.env.BUCKET_NAME = "bar"
+
+    s3Client.promise = jest.fn(async () => true)
+
+    const event = {body: JSON.parse(fs.readFileSync('test_files/test_payload_small.json', 'utf8'))}
+    const response = await handler(event)
+
+    expect(s3Client.putObject).toHaveBeenCalledWith(expect.objectContaining({
+      Bucket: process.env.BUCKET_NAME,
+      Key: expect.any(String),
+      Body:  JSON.stringify(event.body)
+    }))
+
+    expect(response).toStrictEqual({
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: JSON.stringify({ "status" : "RECORD DROPPED" })
+    })
+
+    delete process.env.TABLE_NAME
+    delete process.env.SPLIT_THRESHOLD
+    delete process.env.BUCKET_NAME
   })
 
   it("saves large event body successfully after splitting", async () => {
